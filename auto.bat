cf641ccfc145d86e0049c0bcabfe2094bbf4a51c
joseagjunior <dev.joseagjunior@gmail.com>
2022-11-10 19:37:37 -0300

    v2.3

--- auto.bat
+++ auto.bat
@@ -3,3 +3,17 @@
 @pip install pandas
 
 @pip install tkinter
+
+start criaTabHospital.py
+start criaTabPaciente.py
+start criaTabEnfermeiro.py
+start criaTabMedico.py
+start criaTabTratamento.py
+start criaTabTelefoneMed.py
+start criaTabEspecialidade.py
+start criaTabEspMed.py
+start criaTabAgendamento.py
+start hospital_medico.py
+start hospital_enfermeiro.py
+start medico_enfermeiro.py
+start medico_paciente.py
--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 import os
-
 os.startfile("auto.bat")
 
-import criaTabHospital
-import criaTabPaciente
-import criaTabEnfermeiro
-import criaTabMedico
-import CriaTabTratamento
-import criaTabTelefoneMed
-import criaTabEspecialidade
-import criaTabEspMed
-import criaTabAgendamento
-import hospital_medico
-import hospital_enfermeiro
-import medico_enfermeiro
-import medico_paciente
 import project
